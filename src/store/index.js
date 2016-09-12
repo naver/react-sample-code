7178987f2e290390e56e42c982570a2ac7382c04
YongWoo Jeon <yongwoo.jeon@navercorp.com>
2016-09-12 18:53:22 +0900

    Add devtools #6

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,12 @@
 // middleware 설정
 // 초기값 설정
 import { createStore, compose, applyMiddleware } from "redux";
-import promise from 'redux-promise';
-import createLogger from 'redux-logger';
 import thunk from "redux-thunk";
 
 export default function configureStore(reducer, initialState = {}) {
-	const logger = createLogger();
 	const enhancer = compose(
-		applyMiddleware(thunk,promise, logger/* , middleware 추가*/)
+		applyMiddleware(thunk),
+		window.devToolsExtension ? window.devToolsExtension() : f => f
 	);
 	return createStore(reducer, initialState, enhancer);
 }
-
